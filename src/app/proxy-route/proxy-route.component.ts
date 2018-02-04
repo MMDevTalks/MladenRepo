fdd9564ed9aa0ae272dcdb46107a8f09c8d3e373
Mladen Raev <mladensr@gmail.com>
2018-02-05 00:02:35 +0200

    fix sidebar

--- src/app/proxy-route/proxy-route.component.ts
+++ src/app/proxy-route/proxy-route.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mm-proxy-route',
-  templateUrl: './proxy-route.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./proxy-route.component.scss']
 })
 export class ProxyRouteComponent implements OnInit {
